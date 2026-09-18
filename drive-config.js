27cd85db20bffa73eb50de625b0f99157169059a
kiuja78 <kiuja78@gmail.com>
2026-09-18 14:21:56 +0900

    Add files via upload

--- drive-config.js
+++ drive-config.js
@@ -1,8 +1,7 @@
-// Google Drive 자동저장 기본 설정 (V11.01)
-// 사용자설정 화면에는 Drive 연결 정보를 노출하지 않습니다.
-// Apps Script URL, Drive 폴더 ID, 저장 토큰은 프로그램 내부에서 사용합니다.
+// Google Drive 자동저장 일시 중단 상태입니다.
+// 백업 구조를 다시 설계한 뒤 별도 설정으로 재개합니다.
 window.MJ_DRIVE_CONFIG = {
-  url: "https://script.google.com/macros/s/AKfycbxFmtRqt1qG_3Mk8VeU-vaKYSoLSEnD0CbyaTs5bBZI7IM_ODOHgNGb0_ye37XL43RO/exec",
-  folderId: "1kliszoTfXM_mZLLVI5aWwcV2b88UefS4",
-  token: "b4a563ec89234149b4cd96ab471cbf27d85e1c721c224ed6982508ad92b491b4"
+  url: "",
+  folderId: "",
+  token: ""
 };
